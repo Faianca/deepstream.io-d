5325262316696655edb5a4849cbc798f1ae4bf8a
Jorge Meireles <jorgefaianca@gmail.com>
2016-05-13 15:10:03 +0200

    remove node modules

--- node_modules/deepstream.io/src/default-plugins/noop-storage.js
+++
@@ -1,7 +0,0 @@
-exports.get = function( key, callback ) {
-	callback( null, null );
-};
-
-exports.set = function(){};
-exports.delete = function( key, callback ){ callback( null ); };
-exports.isReady = true;
